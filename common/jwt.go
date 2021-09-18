78924a5f52b278936d779dbfebbdd4fa5e0adf6d
leegogo199 <lifuquan16@nudt.edu.cn>
2021-09-18 16:13:36 +0800

    5th version

---
+++ common/jwt.go
@@ -0,0 +1,34 @@
+package common
+
+import (
+	"GINVUE/Model"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+)
+
+var jwtKey = []byte("a_secret_crect")
+
+type Claims struct {
+	UserId uint
+	jwt.StandardClaims
+}
+
+func ReleaseToken(user Model.User) (string, error) {
+	expirationTime := time.Now().Add(1 * 24 * time.Hour)
+	claims := &Claims{
+		UserId: user.ID,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: expirationTime.Unix(),
+			IssuedAt:  time.Now().Unix(),
+			Issuer:    "GIN01",
+			Subject:   "user token",
+		},
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	tokenString, err := token.SignedString(jwtKey)
+	if err != nil {
+		return " ", err
+	}
+	return tokenString, nil
+}
